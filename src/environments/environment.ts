dd185a5ef957eae857993bb98fe4f1ede0817e32
Kleber Magno <klebermagno@gmail.com>
2023-09-21 09:24:59 -0300

    remove api_key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,11 @@
 
-
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const secret = process.env['FIREBASE_API_KEY'];
-
 export const environment = {
   firebase: {
     projectId: 'itacorubi',
     appId: '1:171683457328:web:c4c521f883a137ffce80f5',
     storageBucket: 'itacorubi.appspot.com',
     locationId: 'us-west2',
-    apiKey: secret,
+    apiKey: 'FIREBASE_API_KEY_PLACEHOLDER',
     authDomain: 'itacorubi.firebaseapp.com',
     messagingSenderId: '171683457328',
     measurementId: 'G-R3EX26S905',
