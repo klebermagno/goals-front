1ebaf22c0658a1309e344250f6f02071becea274
Kleber Magno <klebermagno@gmail.com>
2023-09-21 12:09:56 -0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:171683457328:web:c4c521f883a137ffce80f5',
     storageBucket: 'itacorubi.appspot.com',
     locationId: 'us-west2',
-    apiKey: 'AIzaSyCb2fmX_F-07bYi2wKGGF7VsIiwPbdtQqE',
+    apiKey: 'FIREBASE_API_KEY_PLACEHOLDER',
     authDomain: 'itacorubi.firebaseapp.com',
     messagingSenderId: '171683457328',
     measurementId: 'G-R3EX26S905',
